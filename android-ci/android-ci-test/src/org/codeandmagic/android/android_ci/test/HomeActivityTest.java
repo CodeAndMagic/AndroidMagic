c3f94ab3007fb375a1cdf011bd60f7ea89bcc385
Evelina Vrabie <evelyne24@gmail.com>
2011-11-08 23:21:01 +0000

    Added Robotium

--- android-ci/android-ci-test/src/org/codeandmagic/android/android_ci/test/HomeActivityTest.java
+++ android-ci/android-ci-test/src/org/codeandmagic/android/android_ci/test/HomeActivityTest.java
@@ -1,11 +1,16 @@
 package org.codeandmagic.android.android_ci.test;
 
 import org.codeandmagic.android.android_ci.HomeActivity;
+import org.junit.Assert;
 import org.junit.Test;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import com.jayway.android.robotium.solo.Solo;
+
 public class HomeActivityTest extends ActivityInstrumentationTestCase2<HomeActivity> {
+
+	private Solo mSolo;
 
 	public HomeActivityTest() {
 		super(HomeActivity.class.getPackage().getName(), HomeActivity.class);
@@ -14,10 +19,24 @@ public class HomeActivityTest extends ActivityInstrumentationTestCase2<HomeActiv
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
+		mSolo = new Solo(getInstrumentation(), getActivity());
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		try {
+			mSolo.finalize();
+		}
+		catch (final Throwable e) {
+			e.printStackTrace();
+		}
+		getActivity().finish();
+		super.tearDown();
 	}
 
 	@Test
 	public void testHomeActivity() {
 		assertNotNull(getActivity());
+		Assert.assertTrue(mSolo.searchText("Hello World!"));
 	}
 }
